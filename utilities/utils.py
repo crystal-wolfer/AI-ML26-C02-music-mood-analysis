dc37cc45593dcdb0f3c7cdc88d432a357ef8dc51
t_yordanova <97800053+crystal-wolfer@users.noreply.github.com>
2026-04-26 22:14:32 +0300

    DS1 Profiling as base for other DS profiling
    Profiling of DS1 done as a baseline that will be utilized in a similar way to profile the rest of the datasets. More utility functions will be further identified and the code will be refactored before applying to the other data sets

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,33 +1,29 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "6c14cee9-9689-4104-ab8f-15e4d37c271c",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.13.9"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#Utility function that makes sure all kaggle datasets are downloaded and added to the data folder
+import os
+import sys
+
+REQUIRED_FILES = [
+    'high_popularity_spotify_data.csv',
+    'low_popularity_spotify_data.csv',
+    'universal_top_spotify_songs.csv',
+    'mxmh_survey_results.csv',
+    'World-happiness-report-2024.csv',
+    'World-happiness-report-updated_2024.csv'
+]
+
+def check_datasets_availability(data_path=None):
+    if data_path is None:
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        data_path = os.path.join(script_dir, '../data/')
+    
+    missing = [f for f in REQUIRED_FILES 
+               if not os.path.exists(os.path.join(data_path, f))]
+
+    if missing:
+        print("Missing files in data/ folder:")
+        for f in missing:
+            print(f"  - {f}")
+        print("\nSee README.md for download instructions.")
+        raise FileNotFoundError("Missing datasets stopping notebook execution.")
+    else:
+        print("All datasets found, this notebook can be run without any issues.")
